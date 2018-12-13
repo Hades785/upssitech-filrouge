a42d69fb6db8046b71652cc2504f9012f2935de6
Arthur France <france.arthuret@gmail.com>
2018-12-13 12:28:13 +0100

    Debut basique de quelques fonction outils image

---
+++ partie_1/gen_image.c
@@ -0,0 +1,18 @@
+#include "gen_image.h"
+
+unsigned int simplification(Pixel pixel,unsigned char nbBits){
+	unsigned char simpR,simpG,simpB;
+	unsigned char nbBitsD = (8-nbBits)
+	simpR = pixel.R >> nbBitsD;
+	simpG = pixel.G >> nbBitsD;
+	simpB = pixel.B >> nbBitsD;
+	return = (((((unsigned int)simpR) << nbBits) + simpG) << nbBits) + simpB;
+}
+
+Pixel newPixel(unsigned char R,unsigned char G,unsigned char B){
+	Pixel pix;
+	pix.R = R;
+	pix.G = G;
+	pix.B = B;
+	return pix;
+}
