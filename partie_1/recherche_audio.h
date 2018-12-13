bb1474e2ae73fa268bb20adb5128e750886ebe02
Fuzuki <hades785@gmail.com>
2018-12-13 08:29:05 +0100

    Update search headers (add include indexing counterparts)

--- partie_1/recherche_audio.h
+++ partie_1/recherche_audio.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "indexation_audio.h"
 
 void recherche_audio(/*TODO*/);
--- partie_1/recherche_image.h
+++ partie_1/recherche_image.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "indexation_image.h"
 
 void recherche_image(/*TODO*/);
