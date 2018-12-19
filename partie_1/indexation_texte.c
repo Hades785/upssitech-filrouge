7a2532e29d9f1a82347c7ed16111cf879e4269d7
NicoBGithub <45195022+NicoBGithub@users.noreply.github.com>
2018-12-19 14:13:18 +0100

    première modif

--- partie_1/indexation_texte.c
+++ partie_1/indexation_texte.c
@@ -1,1 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "indexation_texte.h"
+
+void indexation_texte(char[] repertoire)
+{
+	FILE* fich = NULL;
+
+    fich = fopen("test.txt", "r");
+	
+	if (fichier != NULL)
+    {
+
+        // On lit le fichier
+
+        fclose(fichier);
+
+    }
+	else
+	{
+		printf("Impossible d'ouvrir le fichier test.txt");
+	}
+}
