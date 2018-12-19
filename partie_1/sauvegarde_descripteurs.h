bdb5f16fa4aeba1a8c5839399bcd8567389f33dc
Arthur France <france.arthuret@gmail.com>
2018-12-19 15:34:32 +0100

    const and sds update

--- partie_1/sauvegarde_descripteurs.h
+++ partie_1/sauvegarde_descripteurs.h
@@ -1,10 +1,9 @@
-#include <string.h>
-#include "String.h"
+#include "sds.h"
 #define TAILLE_BUF 1000//taille des blocs lus entre chaque malloc du string du descripteur (en gros si tu comprends pas touche pas)
 typedef struct{
 	unsigned int nbDescripteurs;
-	String * descripteurs;
+	sds ** descripteurs;
 }Capsule;
 
-Capsule loadDescripteurs(unsigned char * successFlag,String fichierDescripteur);
-void saveDescripteurs(unsigned char * successFlag,Capsule capsule,String fichierDescripteurs);
+Capsule loadDescripteurs(unsigned char * successFlag,const sds fichierDescripteur);
+void saveDescripteurs(unsigned char * successFlag,const Capsule capsule,const sds fichierDescripteurs);
