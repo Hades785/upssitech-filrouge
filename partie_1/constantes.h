a04094ad12e2fde4a9fabfea15d1b1e6c848108c
Fuzuki <hades785@gmail.com>
2018-12-20 13:25:33 +0100

    Update constantes.h
    Ajout constantes indexation audio

--- partie_1/constantes.h
+++ partie_1/constantes.h
@@ -3,4 +3,9 @@
 
 #define NB_RESULTAT_RECHERCHE 10
 
+
+// Constantes Indexation Audio
+#define NB_SAMPLE_WINDOW 1000 // 1000 echantillons -> 1/16 s
+#define NB_AMP_INTERVAL 10   // 10 intervales d'amplitude
+
 #endif
