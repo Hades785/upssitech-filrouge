4b3014193a13dacda3083a662053768188c75077
Fuzuki <hades785@gmail.com>
2018-12-21 08:25:52 +0100

    Update indexation_audio.c
    Indexation fonctionne.
    Programme de test désactivé.
    
    TODO: Séparer programme de test dans un fichier spécifique.

--- partie_1/indexation_audio.c
+++ partie_1/indexation_audio.c
@@ -1,16 +1,11 @@
 #include "indexation_audio.h"
 #include "constantes.h"
 
-#define TEST_INDEX_AUDIO
-
 sds indexation_audio(const sds chemin_fichier) {
     FILE* file = fopen(chemin_fichier, "r");    // Fichier
-    sds desc = sdsempty();                      // Descripteur TODO
+    sds desc = sdsempty();                      // Descripteur
     double amp;                                 // Amplitude
     int desc_frame[NB_AMP_INTERVAL];            // Trame de descripteur (une fenetre d'echantillons)
-
-    // LOG FILE FOR DEBUG WARNING
-    FILE* log = fopen("/home/fuzuki/UPSSITECH/code/upssitech-filrouge/documents/audio/log", "w");
 
     // Scan tout le fichier
     while(!feof(file)) {
@@ -23,21 +18,20 @@ sds indexation_audio(const sds chemin_fichier) {
             fscanf(file, "%lf", &amp);
 
             // Segmentation par amplitude
-            for(int j=0; j<NB_AMP_INTERVAL; ++j) {
-                if(-1+(j)*((float)2/NB_AMP_INTERVAL) < amp && amp < -1+(j+1)*((float)2/NB_AMP_INTERVAL)) {
+            for(int j=0; j<NB_AMP_INTERVAL; ++j)
+                if(-1+(j)*((float)2/NB_AMP_INTERVAL) < amp && amp < -1+(j+1)*((float)2/NB_AMP_INTERVAL))
                     desc_frame[j]++;
-                }
-            }
         }
 
         // Transfert la trame descripteur dans le descripteur
-        for(int i=0; i<NB_AMP_INTERVAL; ++i)
-            fprintf(log, "%4d ", desc_frame[i]);
-        fprintf(log, "\n");
+        for(int i=0; i<NB_AMP_INTERVAL; ++i) {
+            sds tmp = sdsfromlonglong(desc_frame[i]);
+            tmp = sdscat(tmp, " ");
+            desc = sdscat(desc, tmp);
+            sdsfree(tmp);
+        }
+        desc = sdscat(desc, "\n");
     }
-
-    // CLOSE DEBUG LOG FILE WARNING
-    fclose(log);
 
     // Close opened file
     fclose(file);
@@ -48,9 +42,20 @@ sds indexation_audio(const sds chemin_fichier) {
 #ifdef TEST_INDEX_AUDIO
 
 int main() {
-    sds path = sdsnew("/home/fuzuki/UPSSITECH/code/upssitech-filrouge/documents/audio/jingle_fi.txt");
+    sds path = sdsnew("/home/fuzuki/UPSSITECH/code/upssitech-filrouge/documents/audio/corpus_fi.txt");
 
     sds s = indexation_audio(path);
+
+//     fprintf(stdout, "%s\n", s);
+//     fprintf(stdout, "%s\n", path);
+
+    // Log file
+    FILE* log = fopen("/home/fuzuki/UPSSITECH/code/upssitech-filrouge/documents/audio/log", "w");
+
+    fprintf(log, "%s", s);
+
+    // Close log file
+    fclose(log);
 
     sdsfree(s);
     sdsfree(path);
