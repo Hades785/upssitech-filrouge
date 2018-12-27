b6ef391cb45244ab2e7fcf201857005bcd353d3f
Arthur France <france.arthuret@gmail.com>
2018-12-27 21:43:42 +0100

    Update TestIndixationImage.c
    formatte ton code PROPREMENT

--- partie_1/TestIndixationImage.c
+++ partie_1/TestIndixationImage.c
@@ -2,34 +2,26 @@
 #include <stdlib.h>
 #include "indexation_image.h"
 
-
-
-
-
 int main ()
-{ 
-  
-   Image im;
-   int * histo;
-   
-   decodeImage(&im,"01.txt");
+{
+	Image im;
+	int * histo;
 
-  
+	decodeImage(&im,"01.txt");
 
-   for(unsigned int y = 0;y < im.tailleY;y++){
-      for(unsigned int x = 0;x < im.tailleX;x++){
-          printf("%u  %u  %u    ", (*(im.image+(x*im.tailleY)+y)).R,(*(im.image+(x*im.tailleY)+y)).G,(*(im.image+(x*im.tailleY)+y)).B);
-          }   
-      printf("\n");
-      }
-       
-      histo=histogramme(im,2);
+	for(unsigned int y = 0;y < im.tailleY;y++){
+		for(unsigned int x = 0;x < im.tailleX;x++){
+			printf("%u  %u  %u    ", (*(im.image+(x*im.tailleY)+y)).R,(*(im.image+(x*im.tailleY)+y)).G,(*(im.image+(x*im.tailleY)+y)).B);
+		}
+		printf("\n");
+	}
 
-      printf("\n");
-    
-   for(unsigned int y = 0;y < pow(2,3*2);y++)
-      printf("%d",*(histo+y));
+	histo=histogramme(im,2);
 
+	printf("\n");
 
- return 0;
+	for(unsigned int y = 0;y < pow(2,3*2);y++)
+		printf("%d",*(histo+y));
+
+	return 0;
 }
