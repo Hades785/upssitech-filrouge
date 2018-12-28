b415cfbeb462b2ea15965c8e021fc057703a312a
Arthur France <france.arthuret@gmail.com>
2018-12-28 11:49:14 +0100

    added comments on recherche_image function

--- partie_1/recherche_image.c
+++ partie_1/recherche_image.c
@@ -1,1 +1,4 @@
 #include "recherche_image.h"
+sds * recherche_image(const sds couleur,const Capsule caps,unsigned int nbResMax){
+	
+}
--- partie_1/recherche_image.h
+++ partie_1/recherche_image.h
@@ -7,12 +7,16 @@
 #include "indexation_image.h"
 
 /**
- * 
+ * renvoie un tableau de sds
+ * chaque sds est un resultat par ordre decroissant de match
+ * un resultat est un chemin absolu vers un fichier
  *
- * @param  PLACEHOLDER
- * @return PLACEHOLDER
+ * @param  couleur : nom de la couleur a chercher
+ * @param  caps : capsule contenant les descripteurs de la base de donnees
+ * @return un tableau de resultats se terminant par un pointeur vers null (de taille nbResMax)
+ * ne pas oublier de free ce tableau ainsi que tout ses sds
  */
-sds * recherche_image(const sds couleur,const sds fichier);
+sds * recherche_image(const sds couleur,const Capsule caps,unsigned int nbResMax);
 
 
 #endif
