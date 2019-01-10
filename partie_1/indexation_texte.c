badb248663988985570d7ed5491bc83ffa1f161a
NicoBGithub <45195022+NicoBGithub@users.noreply.github.com>
2019-01-10 16:31:56 +0100

    Update indexation_texte.c

--- partie_1/indexation_texte.c
+++ partie_1/indexation_texte.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 
@@ -23,6 +24,37 @@ void afficher_tabocc(TabOcc t)
 		printf("%s : %u | ", t.mots[i], t.nbOcc[i]);
 	}
 }
+
+
+
+void ajout_mot(TabOcc *t, sds mot)
+{
+	
+}
+
+
+
+int position_mot_dans_tabocc(TabOcc t, sds mot)
+{
+	int present = 0;
+	int posi = 0;
+	
+	for (unsigned int i = 0 ; i < t.nbEle ; i++)
+	{
+		if(strcmp(t.mots[i], mot) == 0) // Les mots sont les mêmes
+		{
+			present = 1;
+			posi = i;
+		}
+	}
+	
+	if(present) // Si le mot est présent, on retourne sa position
+		return i;
+	else
+		return -1;
+}
+
+
 
 
 
@@ -74,118 +106,16 @@ sds indexation_texte(const sds accesFichier,int valId)
 				fscanf(fichier,"%98[äÄëËïÏöÖüÜÿâÂêÊîÎôÔûÛàÀèÈìÌòÒùÙéçÇæÆœŒa-zA-Z]", &tabMots[1]);
 				motActuel = sdsnew(tabMots);
 				
-				// Traitement :
-				if((int)sdslen(motActuel) > 2) // Si le mot actuel fait au moins 3 lettres
+				int num = position_mot_dans_tabocc(tabocc, motActuel);
+				
+				if(num == -1)
 				{
-					int motPresent = 0;
-					int cpt = 1;
-					
-					while(!motPresent && (cpt < (int)sdslen(chaineTotale)))
-					{
-						// Si la 1ère lettre est la même, et que c'est aussi la 1ère lettre
-						// du mot dans chaineTotale. Pour éviter des mots avec des lettres en
-						// plus au début. Ex : fondé / infondé
-						if((chaineTotale[cpt] == motActuel[0]) && !isalpha(chaineTotale[cpt-1]))
-						{
-							motPresent = 1;
-							int i = 0;
-							
-							printf("La 1ère lettre est la même"); getchar();
-							
-							// On vérifie que les lettres suivantes sont les mêmes
-							while(motPresent && (i < (int)sdslen(motActuel)) && (cpt+i < (int)sdslen(chaineTotale)))
-							{
-								//printf("motActuel[i] : %c / chaineTotale[cpt+i] : %c", motActuel[i], chaineTotale[cpt+i]); getchar();
-								
-								if(motActuel[i] != chaineTotale[cpt+i])
-									motPresent = 0;
-								
-								i++;
-							}
-							
-							// On vérifie que le mot détecté n'est pas le début
-							// d'un autre plus long. Ex : robot / robotique
-							if(cpt+i+1 < (int)sdslen(chaineTotale))
-							{
-								if(chaineTotale[cpt+i] != ' ')
-									motPresent = 0;
-							}
-						}
-						
-						cpt++;
-					}
-					
-					
-					if(motPresent)
-					{
-						printf("Le mot lu est déjà dans le descripteur"); getchar();
-						
-						int nbChiffre = 0;
-						int nbOccurence = 0;
-						int ilsSontTousANeuf = 1;
-						
-						// On se place au début du nombre d'occurence
-						cpt += (int)sdslen(motActuel) + 1;
-						
-						while(isdigit(chaineTotale[cpt]))
-						{
-							if(chaineTotale[cpt] != '9')
-								ilsSontTousANeuf = 0;
-							
-							cpt++;
-							nbChiffre++; // On compte le nombre de chiffres
-						}
-						
-						for(int i = 0 ; i < nbChiffre ; i++) // On traduit le string en int
-						{
-							nbOccurence += ((int)(chaineTotale[cpt-1-i] - '0')) * pow(10,i);
-						}
-						
-						nbOccurence++; // On incrémente le nombre d'occurence du mot
-						
-						
-						
-						if(ilsSontTousANeuf) // On doit mettre un chiffre en plus (Ex : 99 -> 100)
-						{
-							sds newS = sdsempty();
-							newS = sdscatsds(newS, chaineTotale);
-							newS = sdscat(newS, " ");
-							
-							for(int i = cpt+1 ; i < (int)sdslen(newS) ; i++) // On décale d'un rang à droite
-							{
-								newS[i] = chaineTotale[i-1];
-							}
-							
-							chaineTotale = newS;
-							// On est passé par ex : de ...mot2 :99;mot3... à ...mot2 :99;;mot3...
-							// A partir de ';', on a tout décalé à droite d'un rang
-							
-							
-							chaineTotale[cpt-nbChiffre] = '1';
-							
-							for(int i = 1 ; i <= nbChiffre ; i++)
-							{
-								chaineTotale[cpt-nbChiffre+i] = '0';
-							}
-							// Finalement, on passe par ex : de ...mot2 :99;;mot3... à ...mot2 :100;mot3...
-						}
-						else
-						{
-							for(int i = 0 ; i < nbChiffre ; i++) // On insère le int dans le sds
-							{
-								int k = nbOccurence%10;
-								nbOccurence -= k;
-								nbOccurence /= 10;
-								chaineTotale[cpt-1-i] = k + '0';
-							}
-						}
-					}
-					else
-					{
-						chaineTotale = sdscatsds(chaineTotale, motActuel);
-						chaineTotale = sdscat(chaineTotale, " :1;");
-						nbDeMotsRetenus++;
-					}
+					ajout_mot(&tabocc, motActuel);
+					nbDeMotsRetenus++;
+				}
+				else
+				{
+					tabocc.nbOcc[i]++;
 				}
 				
 				nbDeMots++;
@@ -213,22 +143,20 @@ sds indexation_texte(const sds accesFichier,int valId)
 		}
 		while (tabMots[0] != EOF); // fin de fichier
 		
+		fclose(fichier);
 		
 		
+		/*
+		sds s1 = sdsnew("[");
+		s1 = sdscatprintf(s, "%d;%d;%d]", valId, nbDeMots, nbDeMotsRetenus);
 		
+		s1 = sdscatsds(s1, chaineTotale);
 		
+		// On libère l'espace mémoire :
+		sdsfree(motActuel);
+		sdsfree(chaineTotale);
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		return(s1);*/
 		
 		
 		
