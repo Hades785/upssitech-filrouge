a98c54f89b034879f0ff83c5eb56f70ba39b1611
Arthur France <france.arthuret@gmail.com>
2019-01-10 21:36:09 +0100

    Added exclusions

--- partie_1/constantes.h
+++ partie_1/constantes.h
@@ -4,7 +4,7 @@
 #define NB_RESULTAT_RECHERCHE 10
 #define TAILLE_MIN_MOT 3
 #define TAILLE_MAX_MOT 30
-const char[] exclusions = "les des par une son que dans est ces ses"
+const char exclusions[] = "les des par une son que dans est ces ses pour sont faire pas Ces Sat Sep GMT ainsi sera Pour où afin même Cette vers vient";
 
 #define SUCCES 0
 #define ECHEC 1
