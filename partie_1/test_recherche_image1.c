d57067a40d5ff0a53d7588c22907ba9cff9c8026
Moufdi96 <45195016+Moufdi96@users.noreply.github.com>
2019-01-11 09:59:25 +0100

    update

--- partie_1/test_recherche_image1.c
+++ partie_1/test_recherche_image1.c
@@ -1,16 +1,17 @@
-#include "sauvgarde_descripteurs.h"
+#include "sauvegarde_descripteurs.h"
 #include "indexation_image.h"
 
 int main(){
 	unsigned char flag;
+	unsigned char a=3;
 	Capsule caps=newCapsule(&flag);
-	addElement(&caps,indexation_image("../documents/images/rgb/20.txt",10,1,1,2));
-	addElement(&caps,indexation_image("../documents/images/rgb/17.txt",10,1,2,2));
-	addElement(&caps,indexation_image("../documents/images/rgb/32.txt",10,1,3,2));
-	addElement(&caps,indexation_image("../documents/images/rgb/36.txt",10,1,4,2));
-	addElement(&caps,indexation_image("../documents/images/rgb/46.txt",10,1,5,2));
-	addElement(&caps,indexation_image("../documents/images/rgb/37.txt",10,1,6,2));
-	addElement(&caps,indexation_image("../documents/images/rgb/33.txt",10,1,7,2));
+	addElement(&caps,indexation_image("../documents/images/rgb/20.txt",50,1,1,a));
+	addElement(&caps,indexation_image("../documents/images/rgb/17.txt",50,1,2,a));
+	addElement(&caps,indexation_image("../documents/images/rgb/32.txt",50,1,3,a));
+	addElement(&caps,indexation_image("../documents/images/rgb/36.txt",50,1,4,a));
+	addElement(&caps,indexation_image("../documents/images/rgb/46.txt",50,1,5,a));
+	addElement(&caps,indexation_image("../documents/images/rgb/37.txt",50,1,6,a));
+	addElement(&caps,indexation_image("../documents/images/rgb/33.txt",50,1,7,a));
 	saveDescripteurs(&flag,caps,"rechercheimage.desc");
 
 }
