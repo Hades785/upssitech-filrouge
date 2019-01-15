86cc59bb28ce1315ec63f5bb9c848f9b163058d1
Fuzuki <hades785@gmail.com>
2019-01-15 10:53:16 +0100

    WIP
    Need fixing file read

--- partie_1/test_indexation_audio.c
+++ partie_1/test_indexation_audio.c
@@ -1,21 +1,80 @@
+#include <string.h>
 #include "indexation_audio.h"
+#include "sauvegarde_descripteurs.h"
+
+// DEBUG!!!!!!
+#define NB_FILES 10
+#define NB_CHAR_FILENAME 50
+
+
+int get_files(char** files) {
+    char* cmd = "ls ../documents/audio/ | grep \\.txt";    
+    FILE* fp;
+
+    if ((fp = popen(cmd, "r")) == NULL) {
+        // printf("Error opening pipe!\n");
+        return -1;
+    }
+
+    int i = 0;
+    while (!feof(fp)) {
+        fscanf(fp, "%s", files[i]);
+        i++;
+    }
+
+    if(pclose(fp))  {
+        // printf("Command not found or exited with error status\n");
+        return -1;
+    }
+
+    return 0;
+}
 
 int main() {
-    sds path = sdsnew("/home/fuzuki/UPSSITECH/code/upssitech-filrouge/documents/audio/corpus_fi.txt");
+    // Recuperation des noms des fichiers a indexer
+    int file_count = 0;
+    char** files = (char**) malloc(sizeof(char*)*NB_FILES);
+    for(int i = 0; i < NB_FILES; i++)
+        files[i] = (char*) malloc(sizeof(char)*NB_CHAR_FILENAME);
+    get_files(files);
+    for(int i = 0; i < NB_FILES; i++)
+        if(strcmp(files[i], "\0") != 0)
+            file_count++;
 
-    sds s = indexation_audio(path);
+    // Generation de la capsule dse descripteurs
+    sds path = sdsnew("../documents/audio/");
+    char flag;
+    Capsule capsule = newCapsule(&flag);
+    if(flag == SUCCES)
+        for(int i = 0; i < file_count; i++) {
+            sds tmp = sdsdup(path);
+            tmp = sdscat(tmp, files[i]);
 
-//     fprintf(stdout, "%s\n", s);
-//     fprintf(stdout, "%s\n", path);
+            printf("%s\n", tmp);
 
-    // Log file
-    FILE* log = fopen("/home/fuzuki/UPSSITECH/code/upssitech-filrouge/documents/audio/log", "w");
+            sds s = indexation_audio(path); //infinite loop....
+            // addElementCopy(&capsule, s);
 
-    fprintf(log, "%s", s);
+            sdsfree(s);
+            sdsfree(tmp);
+        }
+    freeCapsule(capsule);
 
-    // Close log file
-    fclose(log);
+    // sds s = indexation_audio(path);
 
-    sdsfree(s);
+    // // Log file
+    // FILE* log = fopen("/home/fuzuki/UPSSITECH/code/upssitech-filrouge/documents/audio/log", "w");
+
+    // fprintf(log, "%s", s);
+
+    // // Close log file
+    // fclose(log);
+
+    // sdsfree(s);
+
+    for(int i = 0; i < NB_FILES; i++)
+        free(files[i]);
+    free(files);
+
     sdsfree(path);
 }
