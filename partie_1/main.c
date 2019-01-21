47835ffd614594ebb9eee3d9b71ccad9649a80b1
Arthur France <france.arthuret@gmail.com>
2019-01-21 23:31:57 +0100

    simple menu qui fonctionne
    TODO : remplir avec les menus et les appels de focntions
    savoir si une indexation a déja été faite, et la faire automatiquement si besoin

--- partie_1/main.c
+++ partie_1/main.c
@@ -10,11 +10,13 @@
 #include <sys/stat.h>
 
 enum Etape{
-	ETAPE_DEMANDE_CHEMIN,
+	MENU_DEMANDE_CHEMIN,
 	ETAPE_DEMARRAGE,
 	MENU_PRINCIPAL,
 	MENU_RECHERCHE,
 	MENU_CONFIGURATION,
+	MENU_CONFIG_RECHERCHE,
+	MENU_CONFIG_INDEXATION,
 	ETAPE_INDEXATION
 };
 
@@ -28,6 +30,7 @@ ConfMap defaultConfMap(){
 	assert(flag != ECHEC);
 	flag = addValueLong(&map,"nb_bits_image",4);
 	assert(flag != ECHEC);
+	return map;
 }
 
 int main(){
@@ -43,7 +46,7 @@ int main(){
 	DIR * progDir = opendir(dirPath);
 	if(progDir == NULL){
 		if(errno == ENOENT){
-			if(mkdir(dirPath,0x777) == -1){
+			if(mkdir(dirPath,S_IRWXU) == -1){
 				perror("Erreur fatale ");
 				sdsfree(dirPath);
 				return -1;
@@ -57,27 +60,28 @@ int main(){
 	fputs(".",stdout);
 	
 	ConfMap map;
+	unsigned char mapModif = 0;
 	
 	//on regarde si le fichier de configuration existe
 	if(config_file_exists() == 0){
 		if(errno == ENOENT){
 			map = defaultConfMap();
-			flag = save_config_file(map);
+			flag = save_config_file(map);//pas de mapmodif car on la save direct
 			if(flag != SUCCES){
-				perror("Erreur fatale ");
+				perror("Erreur fatale 1 ");
 				sdsfree(dirPath);
 				freeConfMap(map);
 				return -1;
 			}
 		}else{
-			perror("Erreur fatale ");
+			perror("Erreur fatale 2 ");
 			sdsfree(dirPath);
 			return -1;
 		}
 	}else{
 		map = read_config_file(&flag);
 		if(flag != SUCCES){
-			perror("Erreur fatale ");
+			perror("Erreur fatale 3 ");
 			sdsfree(dirPath);
 			freeConfMap(map);
 		}
@@ -89,14 +93,14 @@ int main(){
 	unsigned char presence_key_chemin = 0;
 	long pos = keyPosition(&map,"chemin_bdd");
 	if(pos == -1){
-		etape = ETAPE_DEMANDE_CHEMIN;
+		etape = MENU_DEMANDE_CHEMIN;
 	}else{
 		dirBdd = opendir(map.values->descripteurs[pos]);
 		if(dirBdd == NULL){
 			if(errno == ENOENT){
-				etape = ETAPE_DEMANDE_CHEMIN;
+				etape = MENU_DEMANDE_CHEMIN;
 			}else{
-				perror("Erreur fatale ");
+				perror("Erreur fatale 4 ");
 				sdsfree(dirPath);
 				freeConfMap(map);
 				return -1;
@@ -111,30 +115,41 @@ int main(){
 	
 	do{
 		switch(etape){
-			case ETAPE_DEMANDE_CHEMIN:
+			case MENU_DEMANDE_CHEMIN:
 				puts("Entrer le chemin vers la base de données :");
 				puts("(soit en relatif si l'execution se fait toujours depuis le même répertoire, soit en absolu.)");
 				puts("(Rappel : sur WSL, les disques sont en /mnt/)");
+				if(presence_key_chemin) puts("(entrer '0' retournera au menu de configuration)");
 				scanf("%300s",buf);
-				dirBdd = opendir(buf);
-				if(dirBdd == NULL){
-					if(errno != ENOENT){
-						perror("Erreur fatale ");
-						running = 0;
-					}
-				}else if(presence_key_chemin){
-					changeValue(&map,"chemin_bdd",buf);
+				if(buf[0] == '0' && presence_key_chemin){
+					puts("Annulation");
+					etape = MENU_CONFIGURATION;
 				}else{
-					addValue(&map,"chemin_bdd",buf);
-					presence_key_chemin = 1;
-					etape = MENU_PRINCIPAL;
+					dirBdd = opendir(buf);
+					if(dirBdd == NULL){
+						if(errno != ENOENT){
+							perror("Erreur fatale 5 ");
+							running = 0;
+						}else{
+							printf("Répertoire non trouvé : %s\n",buf);
+						}
+					}else if(presence_key_chemin){
+						changeValue(&map,"chemin_bdd",buf);
+						mapModif = 1;
+						etape = MENU_PRINCIPAL;
+					}else{
+						addValue(&map,"chemin_bdd",buf);
+						mapModif = 1;
+						presence_key_chemin = 1;
+						etape = MENU_PRINCIPAL;
+					}
 				}
 				break;
 			case ETAPE_DEMARRAGE:
 				etape = MENU_PRINCIPAL;//TODO
 				break;
 			case MENU_PRINCIPAL:
-				puts("MENU PRINCIPAL");
+				puts("\nMENU PRINCIPAL");
 				puts("1-Recherche");
 				puts("2-Configuration");
 				puts("3-Réindexation");
@@ -145,6 +160,21 @@ int main(){
 					case '2': etape = MENU_CONFIGURATION; break;
 					case '3': etape = ETAPE_INDEXATION; break;
 					case '0': running = 0; break;
+					case '*': puts("Crédits : Loïc Bréard, Arthur France, Ludivine Albertus, Moufdi Nom trop long tu le tapera toi meme, Nicolas Boirel"); break;
+				}
+				break;
+			case MENU_CONFIGURATION:
+				puts("\nMENU DE CONFIGURATION");
+				puts("1-Modifier le chemin vers la BDD");
+				puts("2-Modifier les parametres de recherche");
+				puts("3-Modifier les parametres d'indexation");
+				puts("\n0-Retour MENU PRINCIPAL");
+				scanf("%1s",buf);
+				switch(buf[0]){
+					case '1': etape = MENU_DEMANDE_CHEMIN; break;
+					case '2': etape = MENU_CONFIG_RECHERCHE; break;
+					case '3': etape = MENU_CONFIG_INDEXATION; break;
+					case '0': etape = MENU_PRINCIPAL; break;
 				}
 				break;
 			default:
@@ -153,6 +183,14 @@ int main(){
 				break;
 		}
 	}while(running);
+	if(mapModif){
+		flag = save_config_file(map);
+		if(flag != SUCCES){
+			perror("Erreur sauvegarde fichier config ");
+		}
+	}
+	puts("A bientôt !");
+	closedir(progDir);closedir(dirBdd);
 	sdsfree(dirPath);
 	freeConfMap(map);
 }
