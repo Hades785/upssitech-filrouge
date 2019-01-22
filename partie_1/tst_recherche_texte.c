f61b9a4f02d4688045f5f9da68f3b91d36893a6b
Ludichat <45141456+Ludichat@users.noreply.github.com>
2019-01-22 18:32:44 +0100

    recherche_texte terminee

--- partie_1/tst_recherche_texte.c
+++ partie_1/tst_recherche_texte.c
@@ -1,19 +1,30 @@
 #include "recherche_texte.h"
 #include "sds.h"
 #include "constantes.h"
+#include "sauvegarde_descripteurs.h"
+#include "indexation.h"
 
 int main()
 {
+	unsigned char flag;
+	Capsule liste_base_texte = loadDescripteurs(&flag, "index.txt");
+	Capsule table_index = loadDescripteurs(&flag, "table_index.txt");
 	printf("Begin test recherche par mots cles\n\n");
 	
-	recherche_texte_motscles("chat ", "index.txt", "table_index.txt");
+	recherche_texte_motscles("chat fichier texte indexer patate ", liste_base_texte, table_index, 10);
 	
 	printf("\nTest ended\n");
 	
 	printf("Begin test recherche par fichiers\n\n");
 	
-	recherche_texte_fichier("test.txt", "index.txt", "descripteurs.txt");
+	sds descripteur = indexation_texte("test.txt", 0);
+	recherche_texte_fichier(descripteur, liste_base_texte, table_index, 10);
 	
 	printf("\nTest ended\n");
+	
+	freeCapsule(liste_base_texte);
+	freeCapsule(table_index);
+	sdsfree(descripteur);
+	
 	return 0;
 }
