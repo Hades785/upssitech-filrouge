b274b6884cfb863ef53d71f6180dbb95573d7da9
Arthur France <france.arthuret@gmail.com>
2019-01-23 16:41:37 +0100

    Added image indexation call

--- partie_1/indexation.c
+++ partie_1/indexation.c
@@ -5,6 +5,10 @@
 #include <assert.h>
 #include <string.h>
 
+sds getDirPath(){
+	return sdscat(sdscat(sdsnew(getenv("HOME")),"/"),STORAGE_FOLDER_NAME);
+}
+
 void getAllFiles(const char * chemin_fichiers,const char * extension,Capsule * caps){
 	printf("Indexation dossier : %s\n",chemin_fichiers);
 	DIR * fileDirectory = opendir(chemin_fichiers);
@@ -31,23 +35,46 @@ void getAllFiles(const char * chemin_fichiers,const char * extension,Capsule * c
 }
 
 void indexer_texte(const char * chemin_desc,const ConfMap * map){
+	puts("ENCORE A FAIRE !!!!!!!");
 }
 
 void indexer_image(const char * chemin_desc,const ConfMap * map){
 	unsigned char flag;
 	unsigned int nbCouleurs = getConfigValueLong(map,"nb_couleurs_max",&flag);
 	if(flag != SUCCES){
-		puts("Erreur parametres");
+		puts("Erreur parametre");
 		assert(flag != ECHEC);
 	}
+	
+	float seuil = getConfigValueFloat(map,"seuil_couleur_image",&flag);
+	if(flag != SUCCES){
+		puts("Erreur parametre");
+		assert(flag != ECHEC);
+	}
+	
+	unsigned char nbBits = getConfigValueLong(map,"nb_bits_image",&flag);
+	if(flag != SUCCES){
+		puts("Erreur parametre");
+		assert(flag != ECHEC);
+	}
+	
 	Capsule descs = newCapsule(&flag);
 	Capsule fichiers = newCapsule(&flag);
 	getAllFiles(getConfigValue(map,"chemin_bdd_im",&flag),".txt",&fichiers);
 	for(unsigned int i = 0;i < fichiers.nbDescripteurs;i++){
-		addElement(&descs,indexation_image(fichiers.descripteurs[i],)
+		addElement(&descs,indexation_image(fichiers.descripteurs[i],nbCouleurs,seuil,i,nbBits));
+	}
+	sds chemin = sdscat(sdscat(getDirPath(),"/"),NOM_FICH_DESC_IMG);
+	saveDescripteurs(&flag,descs,chemin);
+	sdsfree(chemin);
+	freeCapsule(descs);
+	freeCapsule(fichiers);
+	if(flag != SUCCES){
+		puts("Erreur seuvegarde descripteurs");
+		assert(flag != ECHEC);
 	}
 }
 
 void indexer_audio(const char * chemin_desc,const ConfMap * map){
-	
+	puts("ENCORE A FAIRE !!!!!!!");
 }
