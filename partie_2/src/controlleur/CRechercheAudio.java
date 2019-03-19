5a11346e9fac08e4fad85d442da4201d9dca73c9
Ludichat <45141456+Ludichat@users.noreply.github.com>
2019-03-19 09:31:44 +0100

    ajout controleurs
    les controleurs faisant appel à la JNI
    Il faudra ajouter le nom de la classe correspondante devant chaque fonction de la JNI

---
+++ partie_2/src/controlleur/CRechercheAudio.java
@@ -0,0 +1,26 @@
+package controlleur;
+
+import java.util.ArrayList;
+
+public class CRechercheAudio {
+
+	public ArrayList<String> rechercherAudio(String chemin)
+	{
+		String resultats;
+		ArrayList<String> resFormattes = new ArrayList<>();
+		
+		//TODO ajouter nom classe de la methode statique
+		resultats = rechercherAudio(chemin);
+		
+		// separations des resultats
+		String[] tmp = resultats.split("\n");
+		
+		// stockage sous forme de liste
+		for(int i = 0; i < tmp.length; i++)
+		{
+			resFormattes.add(tmp[i]);
+		}
+		
+		return resFormattes;
+	}
+}
---
+++ partie_2/src/controlleur/CRechercheTexteFichier.java
@@ -0,0 +1,26 @@
+package controlleur;
+
+import java.util.ArrayList;
+
+public class CRechercheTexteFichier {
+
+	public ArrayList<String> rechercherFichier(String chemin)
+	{
+		String resultats;
+		ArrayList<String> resFormattes = new ArrayList<>();
+		
+		//TODO ajouter nom classe de la methode statique
+		resultats = rechercherTexte(chemin);
+		
+		// separations des resultats
+		String[] tmp = resultats.split("\n");
+		
+		// stockage sous forme de liste
+		for(int i = 0; i < tmp.length; i++)
+		{
+			resFormattes.add(tmp[i]);
+		}
+		
+		return resFormattes;
+	}
+}
