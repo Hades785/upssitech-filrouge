92650dbeee012444507c040e7f6b956d5f8367cf
Fuzuki <hades785@gmail.com>
2019-03-21 17:29:34 +0100

    Update Tester.java
    Update to use new signatures

--- partie_2/src/jni/Tester.java
+++ partie_2/src/jni/Tester.java
@@ -6,15 +6,15 @@ public class Tester {
 	}
 
 	public static void main(String[] args) {
-		MoteurC.indexerTexte(15, 3);
-		MoteurC.indexerImage(3, 255, 8);
-		MoteurC.indexerAudio(1000, 10);
+		MoteurC.indexerTexte("BDD", "MAP", "ID", 15, 3);
+		MoteurC.indexerImage("BDD", 3, 255, 8);
+		MoteurC.indexerAudio("BDD", 1000, 10);
 		
-		String a = MoteurC.rechercherTexte("FICHIER", 5);
-		String b = MoteurC.rechercherMots("+Chat -Chien", 5);
-		String c = MoteurC.rechercherImage("FICHIER", 5, 8);
-		String d = MoteurC.rechercherCouleur(4, 5, 8);
-		String e = MoteurC.rechercherAudio("FICHIER", 5, 1000, 10);
+		String a = MoteurC.rechercherTexte("BDD", "MAP", "FICHIER", 5);
+		String b = MoteurC.rechercherMots("MAP", "ID", "+Chat -Chien", 5);
+		String c = MoteurC.rechercherImage("BDD", "FICHIER", 5, 8);
+		String d = MoteurC.rechercherCouleur("BDD", 4, 5, 8);
+		String e = MoteurC.rechercherAudio("BDD", "FICHIER", 5, 1000, 10);
 
 		System.out.println(a);
 		System.out.println(b);
