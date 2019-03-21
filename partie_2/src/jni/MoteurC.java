19fc8550dd1e2d535a9ae5c91ab7f728a9556141
Arthur France <france.arthuret@gmail.com>
2019-03-21 16:59:14 +0100

    Updated jni functions
    Ajout des chemins vers les bdd nécessaire au fonctionnement

--- partie_2/src/jni/MoteurC.java
+++ partie_2/src/jni/MoteurC.java
@@ -11,80 +11,80 @@ public class MoteurC {
 		initTest();
 	}
 	
-	private static native void indexerTexteC(int nbMotsMax,int tailleminMot);
-	private static void indexerTexteTest() {
+	private static native void indexerTexteC(String cheminBdd,String cheminMap,String cheminId,int nbMotsMax,int tailleminMot);
+	private static void indexerTexteTest(String cheminBdd,String cheminMap,String cheminId,int nbMotsMax,int tailleminMot) {
 		System.out.println("indexationTexteC");
 		//TODO
 	}
-	public static void indexerTexte() {
-		indexerTexteTest();
+	public static void indexerTexte(String cheminBdd,String cheminMap,String cheminId,int nbMotsMax,int tailleminMot) {
+		indexerTexteTest(cheminBdd,cheminMap,cheminId,nbMotsMax,tailleminMot);
 	}
 	
-	private static native void indexerImageC(int nbCouleursMax,float seuilCouleur,int nbBits);
-	private static void indexerImageTest(int nbCouleursMax,float seuilCouleur,int nbBits) {
+	private static native void indexerImageC(String cheminBdd,int nbCouleursMax,float seuilCouleur,int nbBits);
+	private static void indexerImageTest(String cheminBdd,int nbCouleursMax,float seuilCouleur,int nbBits) {
 		System.out.println("indexationImageC");
 		//TODO
 	}
-	public static void indexerImage(int nbCouleursMax,float seuilCouleur,int nbBits) {
-		indexerImageTest(nbCouleursMax,seuilCouleur,nbBits);
+	public static void indexerImage(String cheminBdd,int nbCouleursMax,float seuilCouleur,int nbBits) {
+		indexerImageTest(cheminBdd,nbCouleursMax,seuilCouleur,nbBits);
 	}
 
-	private static native void indexerAudioC(int nbEchantillonPFenetre,int nbIntAmp);
-	private static void indexerAudioTest(int nbEchantillonPFenetre,int nbIntAmp) {
+	private static native void indexerAudioC(String cheminBdd,int nbEchantillonPFenetre,int nbIntAmp);
+	private static void indexerAudioTest(String cheminBdd,int nbEchantillonPFenetre,int nbIntAmp) {
 		System.out.println("indexationAudioC");
 		//TODO
 	}
-	public static void indexerAudio(int nbEchantillonPFenetre,int nbIntAmp) {
-		indexerAudioTest(nbEchantillonPFenetre, nbIntAmp);
+	public static void indexerAudio(String cheminBdd,int nbEchantillonPFenetre,int nbIntAmp) {
+		indexerAudioTest(cheminBdd,nbEchantillonPFenetre, nbIntAmp);
 	}
 
-	private static native String rechercherTexteC(String cheminFichier,int nbRes);
-	private static String rechercherTexteTest(String cheminFichier,int nbRes) {
+	private static native String rechercherTexteC(String cheminMap,String cheminId,String cheminFichier,int nbRes);
+	private static String rechercherTexteTest(String cheminMap,String cheminId,String cheminFichier,int nbRes) {
 		System.out.println("rechercheTexteC");
 		//TODO
 		return "Attention\nFonction non faite";
 	}
-	public static String rechercherTexte(String cheminFichier,int nbRes) {
-		return rechercherTexteTest(cheminFichier, nbRes);
+	public static String rechercherTexte(String cheminMap,String cheminId,String cheminFichier,int nbRes) {
+		return rechercherTexteTest(cheminMap,cheminId,cheminFichier, nbRes);
 	}
 
-	private static native String rechercherMotsC(String mots,int nbRes);
-	private static String rechercherMotsTest(String mots,int nbRes) {
+	private static native String rechercherMotsC(String cheminMap,String cheminId,String mots,int nbRes);
+	private static String rechercherMotsTest(String cheminMap,String cheminId,String mots,int nbRes) {
 		System.out.println("RechercheMotsC");
 		//TODO
 		return "Attention\nFonction non faite";
 	}
-	public static String rechercherMots(String mots,int nbRes) {
-		return rechercherMotsTest(mots, nbRes);
+	public static String rechercherMots(String cheminMap,String cheminId,String mots,int nbRes) {
+		return rechercherMotsTest(cheminMap,cheminId,mots, nbRes);
 	}
 
-	private static native String rechercherImageC(String cheminImage,int nbRes,int nbBits);
-	private static String rechercherImageTest(String cheminImage,int nbRes,int nbBits) {
+	private static native String rechercherImageC(String cheminBdd,String cheminImage,int nbRes,int nbBits);
+	private static String rechercherImageTest(String cheminBdd,String cheminImage,int nbRes,int nbBits) {
 		System.out.println("RechercheImageC");
 		//TODO
 		return "Attention\nFonction non faite";
 	}
-	public static String rechercherImage(String cheminImage,int nbRes,int nbBits) {
-		return rechercherImageTest(cheminImage, nbRes, nbBits);
+	public static String rechercherImage(String cheminBdd,String cheminImage,int nbRes,int nbBits) {
+		return rechercherImageTest(cheminBdd,cheminImage, nbRes, nbBits);
 	}
 
-	private static native String rechercherCouleurC(int colorCode,int nbRes,int nbBits);
-	private static String rechercherCouleurTest(int colorCode,int nbRes,int nbBits) {
+	private static native String rechercherCouleurC(String cheminBdd,int colorCode,int nbRes,int nbBits);
+	private static String rechercherCouleurTest(String cheminBdd,int colorCode,int nbRes,int nbBits) {
 		System.out.println("RechercheCouleurC");
 		//TODO
 		return "Attention\nFonciton non faite";
 	}
-	public static String rechercherCouleur(int colorCode,int nbRes,int nbBits) {
-		return rechercherCouleurTest(colorCode, nbRes, nbBits);
+	public static String rechercherCouleur(String cheminBdd,int colorCode,int nbRes,int nbBits) {
+		return rechercherCouleurTest(cheminBdd,colorCode, nbRes, nbBits);
 	}
 
-	private static native String rechercherAudioC(String cheminFichier,int winStep,int nbSampWin,int nbIntAmp);
-	private static String rechercherAudioTest(String cheminFichier,int winStep,int nbSampWin,int nbIntAmp) {
+	private static native String rechercherAudioC(String cheminBdd,String cheminFichier,int winStep,int nbSampWin,int nbIntAmp);
+	private static String rechercherAudioTest(String cheminBdd,String cheminFichier,int winStep,int nbSampWin,int nbIntAmp) {
 		System.out.println("RechercheAudioC");
 		//TODO
 		return "Attention\nFonction non faite";
 	}
-	public static String rechercherAudio(String cheminFichier,int winStep,int nbSampWin,int nbIntAmp) {
-		return rechercherAudioTest(cheminFichier, winStep, nbSampWin, nbIntAmp);
+	public static String rechercherAudio(String cheminBdd,String cheminFichier,int winStep,int nbSampWin,int nbIntAmp) {
+		return rechercherAudioTest(cheminBdd,cheminFichier, winStep, nbSampWin, nbIntAmp);
 	}
 }
